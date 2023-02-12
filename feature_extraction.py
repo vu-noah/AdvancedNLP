493a353c859b7e5409f5f1a4aa94564bdf315bcd
vu-noah <n.michael@student.vu.nl>
2023-02-12 19:23:49 +0100

    Update

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,19 +4,56 @@
 
 import stanza
 
-# example sentence we can work with
-example_sentence = '''Everyone has the right to an effective remedy by the competent national tribunals for acts 
-violating the fundamental rights granted him by the constitution or by law.'''
 
-# prepare the pipeline and process the example sentence
-nlp = stanza.Pipeline('en')
-doc = nlp(example_sentence)
+def process_text_with_stanza(text):
+    """
+    Process text with an English stanza pipeline.
+    :param str text: a text
+    :return: stanza.Document processed_text
+    """
+    nlp = stanza.Pipeline('en')
+    processed_text: stanza.Document = nlp(text)
 
-def extract_basic_features():
+    return processed_text
+
+
+def extract_features(doc):
+    """
+    Extract feature dictionaries for every word in a stanza.Document object, store them in a list, return them.
+    :param stanza.Document doc: a stanza.Document object containing processed text
+    :return: list[dict[str, Any]] feature_dictionaries
+    """
+    # create a list to store feature dictionaries in
+    feature_dictionaries = []
+
     for sentence in doc.sentences:
-    # print(sentence)
-    for word in sentence.words:
-        # create a feature dictionary for the word
-        feature_dictionary = {''}
-        head_id = word.head - 1
-        print(word.text, 'is', word.deprel, 'to', sentence.words[head_id].text)
+        for word in sentence.words:
+
+            # create a feature dictionary for the word
+            feature_dictionary = {'token': word.text.lower()}
+
+            # get the head of the token
+            head_id = word.head-1
+            feature_dictionary['head'] = sentence.words[head_id].text.lower()
+
+            # append the feature dictionary to the list of feature dictionaries
+            feature_dictionaries.append(feature_dictionary)
+
+    return feature_dictionaries
+
+
+def main(text):
+    """
+    Extract feature dictionaries for every token from a text, print the feature dictionaries.
+    :param text: a text
+    :return: None
+    """
+    processed_text = process_text_with_stanza(text)
+    feature_dictionaries = extract_features(processed_text)
+    print(feature_dictionaries)
+
+
+if __name__ == '__main__':
+    example_sentence = '''Everyone has the right to an effective remedy by the competent national tribunals for acts 
+    violating the fundamental rights granted him by the constitution or by law.'''
+    main(example_sentence)
