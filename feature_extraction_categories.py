f399f0b74bc0bceac6c05021dfa6dcfd8a84957b
basjems <122371354+basjems@users.noreply.github.com>
2023-02-26 16:04:26 +0100

    Update feature_extraction_categories.py
    Rewrite extract_features_to_determine_roles; fix the near-infinity for-loop; integrate feature 6 and 7 into token level feature dicts (rather than sentence level); add feature 8.

--- feature_extraction_categories.py
+++ feature_extraction_categories.py
@@ -6,14 +6,11 @@ import pandas as pd
 import spacy
 from spacy.tokens import Doc
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
 
-
-# noinspection PyAssignmentToLoopOrWithParameter
 def extract_features_to_determine_roles(filepath):
     """
     Extract features for SR candidate tokens (predicted in the previous step).
-
     :param str filepath: the path to the file with the (predicted) candidates
     :return: tuple df, categorical_feature_dicts, sentence_level_feature_dicts, numerical_feature_dicts
     """
@@ -27,7 +24,6 @@ def extract_features_to_determine_roles(filepath):
     # create three empty lists to put the feature dicts in later
     categorical_feature_dicts = []
     numerical_feature_dicts = []
-    sentence_level_feature_dicts = []
             
     # check whether a token has been predicted as a candidate for an SR (if so, the value in 'candidate_prediction' = 1)
     if 'train' in filepath:
@@ -35,27 +31,58 @@ def extract_features_to_determine_roles(filepath):
     elif 'test' in filepath:
         candidate_column = 'candidate_prediction'
 
-    for i, token in enumerate(df['token']):
-        if df[candidate_column][i] == 1:
 
-            # create a dataframe for each sentence (i.e. rows with the same sent_id) in the same order as the original
-            # file
-            for group in df.groupby('global_sent_id', sort=False):  # does this group only have the candidate
-                # predictions in them?
-                sent_df = group[1]
-
-                # Create a list of tokens and predicates, for feature 6) and 7)
-                sentence = list(sent_df['token'])
-                predicates = list(sent_df['PB_predicate'])
+        # create a dataframe for each sentence (i.e. rows with the same sent_id) in the same order as the original
+        # file
+        for group in df.groupby('global_sent_id', sort=False):
+            sent_df = group[1] 
                 
-                count = 0  # needed to extract the predicate_order feature
+            #A. CREATING SENTENCE LEVEL OBJECTS TO HELP WITH FEATURE EXTRACTION ####
 
-                # for each token in the sentence:
-                for j, row in sent_df.iterrows():
+            # Create a list of tokens and predicates, for feature 6) and 7)
+            sentence = list(sent_df['token'])
+            doc = Doc(nlp.vocab, sentence)
+            
+            #Was used for feature 6b (distance to closest predicate). Not sure if still needed
+            predicates = list(sent_df['PB_predicate'])
+                
+            count = 0 # needed to extract the argument_order feature
+                
+            #create a dictionary that maps token IDs to head IDs, for feature XXX)
+            index_head_dict = {token_id:head_id for token_id, head_id in zip(list(sent_df['token_id_in_sent']), list(sent_df['head_id']))}
 
-                    # create 2 dicts to store the categorical and numerical features in later
-                    categorical_feature_dict = {}
-                    numerical_feature_dict = {}
+                
+            #Find the index of the current predicate for feature XXX)
+            #we assume the current predicate is not the head (also used for feature XXX))
+            cur_pred_is_head = False
+            #the iternum column holds a count that represents which copy of the sentence (and thus, which predicate) we are on
+            predicate_iternum = list(sent_df['iternum'])[0]
+            counter = 0
+            #we iterate over the predicate column: we want to find the ith predicate for i == predicate_iternum    
+            for i, row in sent_df.iterrows():
+                    #we are only interested in predicates, so we skip any non-predicates (value '_')
+                if row['PB_predicate'] == '_':
+                    continue
+                else:
+                    if counter == predicate_iternum:
+                        cur_pred_id_in_sent = row['token_id_in_sent']
+                            #if the predicate is also the head, we set cur_pred_is_head to True
+                        if row['head_id'] == 0:
+                            cur_pred_is_head = True
+                            break
+                        else:
+                            counter += 1
+                            
+            #B. ITERATE OVER TOKENS TO ADD THE FEATURES TO THE FEATURE DICT
+
+            # for each token in the sentence:
+            for i, row in sent_df.iterrows():
+                
+                # create 2 dicts to store the categorical and numerical features in later
+                categorical_feature_dict = {}
+                numerical_feature_dict = {}
+                
+                if row[candidate_column] == 1:
 
                     # 1) extract lemma and PoS of the current token
                     categorical_feature_dict['lemma'] = row['lemma'].lower()
@@ -66,24 +93,20 @@ def extract_features_to_determine_roles(filepath):
                     # 2) extract lemma and POS of the head
                     head_id = row['head_id']
 
-                    if head_id.isdigit():
-                        try:
-                            # find row(s) in the dataframe whose token id equals the current token's head id
-                            head_lemmas = sent_df.loc[sent_df['token_id_in_sent'] == int(head_id)]
-                            categorical_feature_dict['lemma_of_head'] = head_lemmas.iloc[0]['lemma'].lower()
-                            categorical_feature_dict['UPOS_of_head'] = head_lemmas.iloc[0]['UPOS']
-                            categorical_feature_dict['POS_of_head'] = head_lemmas.iloc[0]['POS']
-                        # if the current token is the root, the above gives an IndexError; in that case we add 'None'
-                        # to the feature dict
-                        except IndexError:
-                            categorical_feature_dict['lemma_of_head'] = 'token_is_root'
-                            categorical_feature_dict['UPOS_of_head'] = 'token_is_root'
-                            categorical_feature_dict['POS_of_head'] = 'token_is_root'
-                    else:
-                        categorical_feature_dict['lemma_of_head'] = head_id.lower()
-                        categorical_feature_dict['UPOS_of_head'] = head_id
-                        categorical_feature_dict['POS_of_head'] = head_id
-                        
+
+                    try:
+                        # find row(s) in the dataframe whose token id equals the current token's head id
+                        head_lemmas = sent_df.loc[sent_df['token_id_in_sent'] == int(head_id)]
+                        categorical_feature_dict['lemma_of_head'] = head_lemmas.iloc[0]['lemma'].lower()
+                        categorical_feature_dict['UPOS_of_head'] = head_lemmas.iloc[0]['UPOS']
+                        categorical_feature_dict['POS_of_head'] = head_lemmas.iloc[0]['POS']
+                        # if the current token is the root, the above gives an IndexError; in that case we add 'None' to the
+                        # feature dict
+                    except IndexError:
+                        categorical_feature_dict['lemma_of_head'] = 'token_is_root'
+                        categorical_feature_dict['UPOS_of_head'] = 'token_is_root'
+                        categorical_feature_dict['POS_of_head'] = 'token_is_root'
+
                     # 3) extract whether the token is a NE (check whether UPOS is PROPN)
                     if row['UPOS'] == 'PROPN':
                         numerical_feature_dict['is_NE'] = 1
@@ -105,54 +128,80 @@ def extract_features_to_determine_roles(filepath):
                     else:
                         categorical_feature_dict['voice'] = '_'
                         categorical_feature_dict['predicate_order'] = '_'
-
-                    # append the feature dicts to the list
-                    categorical_feature_dicts.append(categorical_feature_dict)
-                    numerical_feature_dicts.append(numerical_feature_dict)
-
-                # 6) get the distance from the token to the closest predicate
-                # create a list of indexes that have a predicate
-                predicate_indices = [i for i, predicate in enumerate(predicates) if predicate != '_']
-
-                # for each index and token in the sentence list:
-                for i, token in enumerate(sentence):
-
-                    # create an empty dict to put the distance feature in later
-                    distance_feature_dict = {}
-
-                    # if the token is a predicate, fill the dict with value 0
-                    if predicates[i] != '_':
-                        distance_feature_dict['distance_to_predicate'] = 0
-
-                    # otherwise, obtain the distance from the token to the closest predicate,
-                    # by calculating the lowest distance between the index of the token on one hand and each of the
-                    # indexes of the predicate_indices list on the other hand
-                    else:
-                        distance = min(abs(i - index) for index in predicate_indices)
-                        distance_feature_dict['distance_to_predicate'] = distance
-
-                    # append the feature dict to the list
-                    sentence_level_feature_dicts.append(distance_feature_dict)
-                
-                # 7) get the NE type of the token
-                # process the pretokenized text with spacy 
-                doc = Doc(nlp.vocab, sentence)
-                
-                for token in nlp(doc):
-        
-                    ner_feature_dict = {}
+                    
+                    # 6a) get the distance to the current predicate
+                    cur_index = row['token_id_in_sent']
+                    distance = cur_pred_id_in_sent-cur_index
+                    numerical_feature_dict['distance_to_predicate'] = distance
+                    
+                    
+                    # 7) get the NE type of the token
+                    for token in nlp(doc):
                     # if the token is a NE, get its NE tag
-                    if token.ent_type_:
-                        ner_feature_dict['NE_type'] = token.ent_type_
+                        if token.ent_type_:
+                            NE_type = token.ent_type_
                     # if the token is not a NE, return 'O'
-                    else:
-                        ner_feature_dict['NE_type'] = 'O'
-                        
-                    # append the feature dict to the list
-                    sentence_level_feature_dicts.append(ner_feature_dict)
+                        else:
+                            NE_type = 'O'
+                            
+                    categorical_feature_dict['NE_type'] = NE_type
+
+        
+                    # 8. Get the dependency path from current token to current predicate
+                    dependency_path_to_pred = []
+
+                    #find the id of the current token
+                    index = row['token_id_in_sent']
+                    #As long as the head_id of the current token is not the id of the current predicate,
+                    #we find the head of the current token and add its dependency label to the list 'dependency_path_to_pred' 
+                    while index != cur_pred_id_in_sent:
+                        #find the current
+                        dependency_path_to_pred.append(list(sent_df['dependency_label'])[index-1])
+                        #if we reach the root (index == 0), we stop the iteration (break)
+                        if index == 0:
+                            #if the current predicate isn't the root, that means we have not reached the current predicate, 
+                            #therefore there is no possible dependency path to the predicate. We create an empty path '[]'
+                            if not cur_pred_is_head:
+                                dependency_path_to_pred = []
+                            break
+                        #if we did not reach the root, we find the next head of the token and continue the while loop
+                        index = index_head_dict[index]
+
+                    categorical_feature_dict['dependency_path_to_pred'] = dependency_path_to_pred
+
+                # append the feature dicts to the list
+                categorical_feature_dicts.append(categorical_feature_dict)
+                numerical_feature_dicts.append(numerical_feature_dict)
+
+
+#                 # 6b) get the distance from the token to the closest predicate
+#                 # create a list of indexes that have a predicate
+#                 predicate_indices = [i for i, predicate in enumerate(predicates) if predicate != '_']
+
+#                 # for each index and token in the sentence list:
+#                 for i, token in enumerate(sentence):
+
+#                     # create an empty dict to put the distance feature in later
+#                     distance_feature_dict = {}
+
+#                     # if the token is a predicate, fill the dict with value 0
+#                     if predicates[i] != '_':
+#                         distance_feature_dict['distance_to_predicate'] = 0
+
+#                     # otherwise, obtain the distance from the token to the closest predicate,
+#                     # by calculating the lowest distance between the index of the token on one hand and each of the
+#                     # indexes of the predicate_indices list on the other hand
+#                     else:
+#                         distance = min(abs(i - index) for index in predicate_indices)
+#                         distance_feature_dict['distance_to_predicate'] = distance
+
+#                     # append the feature dict to the list
+#                     sentence_level_feature_dicts.append(distance_feature_dict)
+                
+
                     
     # return a zip with the three lists filled with feature dicts
-    return df, categorical_feature_dicts, sentence_level_feature_dicts, numerical_feature_dicts
+    return df, categorical_feature_dicts, numerical_feature_dicts
 
 
 # extract the features to determine the SR of the candidates
