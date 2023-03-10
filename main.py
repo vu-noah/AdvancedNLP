82e7fc06f8e7a0b5d5a35fb69b0a7d363151cfe1
basjems <122371354+basjems@users.noreply.github.com>
2023-03-10 12:25:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ import sys
 def main(argv = None):
     if argv == None:
         argv = sys.argv
-    epochs = int(argv[1])
-    batch_size = int(argv[2])
-    mode = argv[3]
-    load_epoch = argv[4]
-    has_gold = bool(argv[5])
+    epochs = int(argv[0])
+    batch_size = int(argv[1])
+    mode = argv[2]
+    has_gold = bool(argv[3])
     
-    fine_tune_bert(epochs, batch_size, mode)
-    make_predictions_with_finetuned_model(batch_size, load_epoch, has_gold, mode)
+    fine_tune_bert(epochs = epochs, batch_size = batch_size, mode = mode)
+    make_predictions_with_finetuned_model(batch_size = batch_size, load_epoch = epochs, has_gold = has_gold, mode = mode)
     
 if __name__ == '__main__':
     main(sys.argv[1:])
     
    # command line example: 
-   # python3 C:\\Users\\User\\AdvancedNLP\\main.py main 5 4 token_type_IDs 5 True
+   # python3 C:\\Users\\User\\AdvancedNLP\\main.py 5 4 token_type_IDs True
    
