470ded42756f6d74118113def32304744fe7ada5
basjems <122371354+basjems@users.noreply.github.com>
2023-03-10 12:21:48 +0100

    Update main.py
    change main to take commandline arguments

--- main.py
+++ main.py
@@ -3,7 +3,23 @@
 
 from train import fine_tune_bert
 from predict import make_predictions_with_finetuned_model
+import sys
 
+def main(argv = None):
+    if argv == None:
+        argv = sys.argv
+    epochs = int(argv[1])
+    batch_size = int(argv[2])
+    mode = argv[3]
+    load_epoch = argv[4]
+    has_gold = bool(argv[5])
+    
+    fine_tune_bert(epochs, batch_size, mode)
+    make_predictions_with_finetuned_model(batch_size, load_epoch, has_gold, mode)
+    
 if __name__ == '__main__':
-    fine_tune_bert(epochs=5, batch_size=4, mode='token_type_IDs')
-    make_predictions_with_finetuned_model(batch_size=4, load_epoch=5, has_gold=True, mode='token_type_IDs')
+    main(sys.argv[1:])
+    
+   # command line example: 
+   # python3 C:\\Users\\User\\AdvancedNLP\\main.py main 5 4 token_type_IDs 5 True
+   
